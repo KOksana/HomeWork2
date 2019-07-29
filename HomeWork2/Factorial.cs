2bfe7df09f7109d57900c06fcfa45a329c6e3ded
Oksana Koval <oksana_koval@epam.com>
2019-07-29 21:06:24 +0300

    implement Fibonacci and Factorial with and without yield

---
+++ HomeWork2/Factorial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace HomeWork2
+{
+    public class Factorial
+    {
+        private class FactorialCalculator : IEnumerable
+        {
+            public bool calculateNext;
+            public IEnumerator GetEnumerator()
+            {
+                int number = 0;
+                int currentResult = 1;
+                calculateNext = true;
+                while (calculateNext)
+                {
+                    currentResult *= ++number;
+                    yield return currentResult;
+                }
+            }
+        }
+
+        public static void PrintFactorialYield(uint numberToCalculate)
+        {
+            FactorialCalculator factorialCalculator = new FactorialCalculator();
+            int counter = 0;
+            int result = 0;
+            foreach (int currentResult in factorialCalculator)
+            {
+                factorialCalculator.calculateNext = ++counter < numberToCalculate;
+                result = currentResult;
+            }
+            Console.WriteLine(result);
+        }
+
+        public static void PrintFactorial(uint numberToCalculate)
+        {
+            int number = 0;
+            int result = 1;
+            int counter = 0;
+            while (counter++ <numberToCalculate)
+            {
+                result *= ++number;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -10,6 +10,32 @@ namespace HomeWork2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter max index of Fibonacci numbers");
+            uint inputNumber;
+            string inputValue = Console.ReadLine();
+
+            if (uint.TryParse(inputValue, out inputNumber))
+            {
+                Console.WriteLine("Fibonacci output:");
+                Fibonacci.PrintFibonacciUntilIndex(inputNumber);
+
+                Console.WriteLine("Fibonacci with Yield output:");
+                Fibonacci.PrintFibonacciUntilIndexYield(inputNumber);
+            }
+
+            Console.WriteLine("Enter number to calculate factorial");
+            inputValue = Console.ReadLine();
+
+            if (uint.TryParse(inputValue, out inputNumber))
+            {
+                Console.WriteLine("Factorial output:");
+                Factorial.PrintFactorial(inputNumber);
+
+                Console.WriteLine("Factorial with Yield output:");
+                Factorial.PrintFactorialYield(inputNumber);
+            }
+
+            Console.Read();
         }
     }
 }
